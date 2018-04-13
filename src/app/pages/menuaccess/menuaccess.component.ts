c9db1eea1983a9ad876a1c6ecbd4d5e666ae19ba
sptclr <sipitacular@gmail.com>
2018-04-13 18:05:50 +0700

    menu access new + index.html

--- src/app/pages/menuaccess/menuaccess.component.ts
+++ src/app/pages/menuaccess/menuaccess.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
+import { NgForm } from "@angular/forms";
+
+import { MenuaccessService } from "../../services/menuaccess.service";
+import { Menuaccess } from "../../models/menuaccess.model";
 
 @Component({
   selector: 'app-menuaccess',
@@ -6,10 +11,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menuaccess.component.css']
 })
 export class MenuaccessComponent implements OnInit {
+  
+  modalRef: BsModalRef;
+  isNew: Boolean = true;
 
-  constructor() { }
+  constructor(private _menuAccessService: MenuaccessService,
+              private modalService: BsModalService,
+  ) { }
 
   ngOnInit() {
+    this.resetForm();
+    this._menuAccessService.getMenuAccesses();
+  }
+
+  resetForm(form?: NgForm){
+    if (form != null){
+      form.reset();
+    }
+    this._menuAccessService.selectedMenuAccess = new Menuaccess();
+  }//end reset
+
+  onSubmit(form: NgForm){
+    //alert(JSON.stringify(form.value));
+    if(form.value._id == null){
+      this._menuAccessService.postMenuAccess(form.value)
+                            .subscribe(data => {
+                              this._menuAccessService.getMenuAccesses();
+                              this.resetForm(form);
+                              this.modalRef.hide()
+                            })
+    } else{
+      this._menuAccessService.patchMenuAccess(form.value._id, form.value)
+                            .subscribe(data => {
+                              this._menuAccessService.getMenuAccesses();
+                              this.resetForm(form);
+                              this.modalRef.hide();
+                            })
+    }
+  }//end submit
+
+  onEdit(template: TemplateRef<any>, _id: String){
+    this._menuAccessService.getMenuAccess(_id);
+    this.openModalEdit(template);
+  }//end edit
+
+  onDelete(_id){
+    this._menuAccessService.deleteMenuAccess(_id)
+        .subscribe(x => {
+          this._menuAccessService.getMenuAccesses();
+          this.modalRef.hide();
+        })
+  }//end delete
+
+  onView(template: TemplateRef<any>, _id: String) {
+    this._menuAccessService.getMenuAccess(_id);
+    this.openModalView(template);
+  }//end view
+
+  openModalAdd(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+    this.resetForm();
+  }
+
+  openModalView(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  openModalEdit(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
 }
--- src/app/services/menuaccess.service.ts
+++ src/app/services/menuaccess.service.ts
@@ -1,10 +1,61 @@
 //khusus module
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Http, Response, Headers, RequestOptions, RequestMethod } from "@angular/http";
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise'
 
 //khusus component
+import { Menuaccess } from '../models/menuaccess.model';
+
 @Injectable()
 export class MenuaccessService {
 
-  constructor() { }
+  private _url = "http://localhost:3000/api/menuaccesses";
 
-}
+  selectedMenuAccess: Menuaccess;
+  selectedMenuAccessList: Menuaccess[];
+
+  constructor(private http: Http) { }
+
+  getMenuAccesses(){
+    this.http.get(this._url)
+        .map((data: Response) => {
+          return data.json() as Menuaccess[];
+        }).toPromise().then(x => {
+          this.selectedMenuAccessList = x;
+        })
+  }//end get all
+
+  postMenuAccess(menuaccess: Menuaccess){
+    const body = JSON.stringify(menuaccess);
+    const headerOptions = new Headers({'Content-Type': 'application/json'});
+    const requestOptions = new RequestOptions({method: RequestMethod.Post, headers: headerOptions});
+    return this.http.post(this._url, body, requestOptions)
+                    .map(x => x.json());
+  }//end post
+
+  getMenuAccess(_id : String){
+    this.http.get(this._url + '/' + _id)
+             .map((data: Response) => {
+               return data.json() as Menuaccess;
+             }).toPromise().then(x => {
+               this.selectedMenuAccess = x;
+             })
+  }//end get by id
+
+  patchMenuAccess(_id, menuaccess: Menuaccess){
+    const data = JSON.stringify(menuaccess);
+    const headerOptions = new Headers({'Content-Type' : 'application/json'});
+    const requestOptions = new RequestOptions({method: RequestMethod.Patch, headers: headerOptions});
+    return this.http.patch(this._url + '/' + _id, menuaccess, requestOptions)
+                  .map(x => x.json())
+  }//end patch menu access
+
+  deleteMenuAccess(_id){
+    return this.http.delete(this._url + '/' + _id)
+                  .map(x => x.json())
+  }//end delete
+
+}//end class MenuAccessService
