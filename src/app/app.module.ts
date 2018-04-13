adf6ccf4656d3f4c115fa1bf7e8e10e61a6b88a9
qalfin <qalfinq@gmail.com>
2018-04-13 14:40:14 +0700

    user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 //module http
 import { HttpClientModule } from '@angular/common/http';
@@ -44,7 +45,8 @@ import { UserService } from './services/user.service';
     MenuComponent,
     EmployeesComponent,
     UsersComponent,
-    HomeComponent
+    HomeComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
@@ -54,6 +56,7 @@ import { UserService } from './services/user.service';
     HttpModule,
     FormsModule,
     BsDropdownModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [
         { provide: APP_BASE_HREF, useValue: '/'},
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, Input } from '@angular/core';
+import { UserService} from '../../services/user.service';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { NgForm } from '@angular/forms';
+import { User} from '../../models/user.model';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +11,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  modalRef : BsModalRef;
+  isNew: Boolean = true;
 
-  constructor() { }
+  constructor(private userService: UserService, private modalService: BsModalService) { }
 
   ngOnInit() {
+    this.resetForm();
+    this.userService.getUsers()
   }
 
+  resetForm(form?: NgForm){
+    if (form != null) {
+      form.reset();
+    }
+    this.isNew = true;
+    this.userService.selectedUser = new User();
+  }
+
+  openModal( template: TemplateRef<any>){
+    this.modalRef = this.modalService.show(template);
+    this.resetForm();
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.value._id == null) {
+      this.userService.postUser(form.value)
+        .subscribe(data => {
+          this.userService.getUsers();
+          this.resetForm(form);
+          this.modalRef.hide();
+        })
+    }else {
+      this.userService.patchUser(form.value._id, form.value)
+        .subscribe(data => {
+          this.userService.getUsers();
+          this.resetForm(form);
+          this.modalRef.hide();
+      });
+    }
+  }
+
+  onEdit(template: TemplateRef<any>, _id: String) {
+    this.userService.getUser(_id);
+    this.openModal(template);
+    this.isNew = false;
+  }
+
+  onView(template: TemplateRef<any>, _id: String) {
+    this.userService.getUser(_id);
+    this.openModal(template);
+    this.isNew = false;
+  }
+
+  onDelete(_id: String) {
+    if ( confirm('Are You Sure to delete thid record?') === true) {
+      this.userService.deleteUser(_id)
+        .subscribe(x => {
+          this.userService.getUsers();
+        });
+    }
+  }
 }
