17cb26157f91a323df2bf49a7e053d04c6746763
fajriashr <fajrisukmaashr@gmail.com>
2018-04-16 11:51:17 +0700

    Company Edit Angular 1

--- src/app/pages/companies/companies.component.ts
+++ src/app/pages/companies/companies.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { CompanyService } from '../../services/company.service';
+import { NgForm } from '@angular/forms';
+import { Company } from '../../models/company.model';
 
 @Component({
   selector: 'app-companies',
@@ -6,10 +10,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./companies.component.css']
 })
 export class CompaniesComponent implements OnInit {
-
-  constructor() { }
+  modalRef: BsModalRef;
+  isNew : Boolean = true;
+  constructor(private companyService: CompanyService, private modalService: BsModalService) { }
 
   ngOnInit() {
+    this.resetForm();
+    this.companyService.getCompanies();
+  }
+  resetForm(form?: NgForm){
+    if (form != null){
+      form.reset();
+    }
+    this.isNew = true;
+    this.companyService.selectedCompany = new Company();
+  }
+  openModal(template: TemplateRef<any>){
+    this.modalRef = this.modalService.show(template);
+    this.resetForm();
+  }
+  onEdit(template: TemplateRef<any>, _id : String){
+    this.companyService.getCompany(_id);
+    this.openModal(template);
+    this.isNew = false;
+  }
+  onSubmit(form: NgForm){
+    if(form.value._id == null){
+      this.companyService.postCompany(form.value)
+          .subscribe(data => {
+            this.companyService.getCompanies();
+            this.resetForm(form);
+            this.modalRef.hide();
+          });
+    }else{
+      this.companyService.patchCompany(form.value._id, form.value)
+          .subscribe(data => {
+            this.companyService.getCompanies();
+            this.resetForm(form);
+            this.modalRef.hide();
+          })
+    }
+  }
+  onDelete(_id : String){
+    if(confirm('Are you sure want to delete this record?') === true){
+      this.companyService.deleteCompany(_id)
+          .subscribe(x => {
+            this.companyService.getCompanies();
+          })
+    }
   }
 
 }
--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -1,10 +1,52 @@
-//Khusus module
 import { Injectable } from '@angular/core';
+import { Company } from '../models/company.model';
+import { HttpClient } from '@angular/common/http';
+import { Http, Response, Headers, RequestOptions, RequestMethod } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise'
 
-//khusus component
 @Injectable()
 export class CompanyService {
 
-  constructor() { }
+  private _url = 'http://localhost:3000/api/companies'
+  selectedCompany : Company;
+  companyList : Company[];
+  constructor(private http : Http) { }
+
+  getCompanies(){
+    this.http.get(this._url)
+            .map((data: Response) => {
+              return data.json() as Company[];
+            }).toPromise().then(x => {
+              this.companyList = x;
+            })
+  }
+
+  getCompany(_id){
+    this.http.get(this._url + '/' + _id)
+            .map((data: Response) => {
+              return data.json() as Company;
+            }).toPromise().then(x => {
+              this.selectedCompany = x;
+            })
+  }
+
+  postCompany(company: Company){
+    const body = JSON.stringify(company);
+    const headerOptions = new Headers({'Content-Type': 'application/json'});
+    const requestOptions = new RequestOptions({method: RequestMethod.Post, headers: headerOptions});
+    return this.http.post(this._url, body, requestOptions).map(x => x.json());
+  }
+
+  patchCompany(_id, company: Company){
+    const headerOptions = new Headers({'Content-Type': 'application/json'});
+    const requestOptions = new RequestOptions({method: RequestMethod.Patch, headers: headerOptions});
+    return this.http.patch(this._url + '/' + _id, company, requestOptions).map(x => x.json());
+  }
+
+  deleteCompany(_id){
+    return this.http.delete(this._url + '/' + _id).map(x => x.json());
+  }
 
 }
